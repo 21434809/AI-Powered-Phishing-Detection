a9f14bde052b0c0557cb39d7c578e3f2ea0de8aa
Rueben  van der Westhuizen <u21434809@tuks.co.za>
2025-03-18 19:30:31 +0200

    Enable CORS in Flask app and update home component to log server response

--- phishing-detection-frontend/src/app/home/home.component.ts
+++ phishing-detection-frontend/src/app/home/home.component.ts
@@ -12,9 +12,9 @@ export class HomeComponent {
 
   constructor(private http: HttpClient) {}
   onSubmit() {
-    console.log('Hello World!');
     this.http.post('http://127.0.0.1:5000/predict', { text: this.emailText })
       .subscribe(response => {
+        console.log(response);
         this.result = response;
       });
   }
