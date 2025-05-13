c4090143d03840fa92e94dd9312c316ef9e96290
Rueben  van der Westhuizen <u21434809@tuks.co.za>
2025-05-13 15:17:05 +0200

    Add TitleComponent for dynamic title rendering; refactor home component to utilize new title component.

--- phishing-detection-frontend/src/app/app.module.ts
+++ phishing-detection-frontend/src/app/app.module.ts
@@ -8,11 +8,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
+import { TitleComponent } from './shared/title/title.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    TitleComponent
   ],
   imports: [
     BrowserModule,
